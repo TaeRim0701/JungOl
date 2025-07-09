ede698783b6fab9965c31d75b31a52116a657bcb
TaeRim0701 <xofla71@naver.com>
2025-07-09 17:26:18 +0900

    7월 9일

--- Array559.java
+++ Array559.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Array559 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double[] average = {85.6, 79.5, 83.1, 80.0, 78.2, 75.0};
+
+        int Start = sc.nextInt();
+        int End = sc.nextInt();
+
+        System.out.println(average[Start - 1] + average[End - 1]);
+    }
 }
