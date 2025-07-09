429515d007d28dc1714fcf636cfb60695356a59e
TaeRim0701 <xofla71@naver.com>
2025-07-09 22:05:22 +0900

    7월 9일

---
+++ Array9073.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Array9073 {
+    public static void main(String[] args) {
+        int[] arr = {95, 75, 85, 100, 50};
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
