75aab6e1320b53549a173532af52de420d270e36
TaeRim0701 <xofla71@naver.com>
2025-07-11 20:17:57 +0900

    7월 11일

---
+++ Function9081.java
@@ -0,0 +1,12 @@
+public class Function9081 {
+    public static void main(String[] args) {
+        line();
+        System.out.println("line 함수를 호출하였습니다.");
+        System.out.println("line 함수를 다시 호출합니다.");
+        line();
+    }
+
+    public static void line() {
+        System.out.println("==============================");
+    }
+}
