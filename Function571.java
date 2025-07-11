c24ca92bd53cfd02ad6cdc41fb392803923911b5
TaeRim0701 <xofla71@naver.com>
2025-07-11 20:23:06 +0900

    7월 11일

---
+++ Function571.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Function571 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        func(n);
+    }
+
+    public static void func(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println("~!@#$^&*()_+|");
+        }
+    }
+}
